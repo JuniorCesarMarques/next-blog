e08de8bc7797f98ae785cececdb5a6fcb2e14beb
JuniorCesarMarques <juniorcms1995@gmail.com>
2025-02-14 17:14:39 -0300

    remoção do jodit para evitar erros no deploy

--- src/app/new-post/page.jsx
+++ src/app/new-post/page.jsx
@@ -1,23 +1,9 @@
-"use client";
-
-import { useState } from "react";
-import TextEditor from "@/components/text-editor/TextEditor";
 
 const NewPost = () => {
-  const [postContent, setPostContent] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Texto salvo:", postContent);
-    // Aqui você enviaria para o backend
-  };
-
-  let test = "test";
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <h2>Criar um novo post</h2>
-      <TextEditor onChange={setPostContent} />
       <button type="submit">Salvar</button>
     </form>
   );
